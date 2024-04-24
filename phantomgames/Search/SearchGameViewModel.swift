f916bf40b83806b2ad56749cdef8f7f06f2ed3fe
AShozi <AShozi@dvtsoftware.com>
2024-04-24 14:33:51 +0200

    Uploading SearchviewModel file

--- phantomgames/Search/SearchGameViewModel.swift
+++ phantomgames/Search/SearchGameViewModel.swift
@@ -1,4 +1,12 @@
+//
+//  SearchGameViewModel.swift
+//  phantomgames
+//
+//  Created by Aphiwe Shozi on 2024/04/20.
+//
+
 import Foundation
+import UIKit
 
 protocol ViewModelDelegate: AnyObject {
     func reloadView()
@@ -7,9 +15,12 @@ protocol ViewModelDelegate: AnyObject {
 
 class SearchGameViewModel {
     
+
+    
     private var repository: SearchGameRepositoryType?
     private weak var delegate: ViewModelDelegate?
-    private(set) var gameList: [GamesModel] = []
+    private(set) var allGameList: [GameModel] = []
+    private(set) var filteredGames: [GameModel] = []
     
     init(repository: SearchGameRepositoryType,
          delegate: ViewModelDelegate) {
@@ -17,22 +28,48 @@ class SearchGameViewModel {
         self.delegate = delegate
     }
     
+    // MARK: Computed Proterties
+    
     var gameListCount: Int {
-        return gameList.count
+        return allGameList.count
     }
     
+    // MARK: Functions
     
+    func game(atIndex: Int) -> GameModel? {
+        return allGameList[atIndex]
+    }
     
     func fetchSearchResults() {
-        repository?.fetchSearchResults(completion: { [weak self] result in
+        repository?.fetchSearchResults { [weak self] result in
             switch result {
             case .success(let searchResults):
-                self?.gameList = searchResults.results
+                self?.allGameList = searchResults
                 self?.delegate?.reloadView()
             case .failure(let error):
                 self?.delegate?.show(error: error.rawValue)
             }
-        })
+        }
     }
 }
+
+//MARK: - Search Functions
+extension SearchGameViewModel{
+    
+    public func inSearchMode(_ searchController: UISearchController) -> Bool {
+        let isActive = searchController.isActive
+        let searchText = searchController.searchBar.text ?? ""
+        return isActive && !searchText.isEmpty
+    }
+    
+    public func updateSearchController(searchBarText: String?) {
+        self.filteredGames = allGameList
+        
+        if let searchText = searchBarText?.lowercased(), !searchText.isEmpty {
+            self.filteredGames = self.allGameList.filter { $0.title.lowercased().contains(searchText) }
+            }
+        self.delegate?.reloadView()
+    }
+    }
+
 
