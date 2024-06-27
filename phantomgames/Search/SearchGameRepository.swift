7c2d0c842c0b4c87eee64c07b08d3e08fc7758b5
AShozi <AShozi@dvtsoftware.com>
2024-06-27 08:50:15 +0200

    search screen game details segue

--- phantomgames/Search/SearchGameRepository.swift
+++ phantomgames/Search/SearchGameRepository.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 typealias SearchGameResult = (Result<[Game], APIError>) -> Void
+typealias TableDetailResult = (Result<GameDetail, APIError>) -> Void
 
 protocol SearchGameRepositoryType: AnyObject {
     func fetchAPIImage(completion: @escaping SearchGameResult)
     func fetchSearchResults(completion: @escaping SearchGameResult)
     func fetchGames(fromURL url: String, completion: @escaping SearchGameResult)
+    func fetchTableDetailResults(id: Int, completion: @escaping (TableDetailResult))
 }
 
 class SearchGameRepository: SearchGameRepositoryType {
@@ -29,4 +31,10 @@ class SearchGameRepository: SearchGameRepositoryType {
     func fetchGames(fromURL url: String, completion: @escaping SearchGameResult) {
         URLSession.shared.request(endpoint: url, method: .GET, completion: completion)
     }
+    
+    func fetchTableDetailResults(id: Int, completion: @escaping (TableDetailResult)) {
+        let urlString = Constants.Endpoints.gameDetail + "\(id)"
+        URLSession.shared.request(endpoint: urlString, method: .GET, completion: completion)
+    }
+    
 }
